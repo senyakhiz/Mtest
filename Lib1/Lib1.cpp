39959c9c37e4efc89d7915d314c7eb3842cff8ba
Ksenya <hksyusha07@gmail.com>
2026-09-11 01:40:00 +0400

    создана структура

--- Lib1/Lib1.cpp
+++ Lib1/Lib1.cpp
@@ -1,9 +1,48 @@
 ﻿#include "pch.h"
 #include "framework.h"
+#include <iostream>
 
 struct Task {
+	int _a;
+	int _b;
+	char _operation;
+	int _answer;
+
+	Task();
+	Task(int a, int b, char operation);
 
 };
+
+Task::Task() {
+	_operation = "+-*"[std::rand() % 3];
+	_a = 1 + std::rand() % 10;
+	_b = 1 + std::rand() % 10;
+	if (_operation == '+') {
+		_answer = _a + _b;
+	}
+	else if (_operation == '-') {
+		_answer = _a - _b;
+	}
+	else {
+		_answer = _a * _b;
+	}
+}
+
+Task::Task(int a, int b, char operation) {
+	_operation = operation;
+	_a = a + std::rand() % (b - a + 1);
+	_b = a + std::rand() % (b - a + 1);
+
+	if (_operation == '+') {
+		_answer = _a + _b;
+	}
+	else if (_operation == '-') {
+		_answer = _a - _b;
+	}
+	else {
+		_answer = _a * _b;
+	}
+}
 
 class MathTest {
 
--- gtest/test.cpp
+++ gtest/test.cpp
@@ -4,3 +4,8 @@ TEST(TestCaseName, TestName) {
   EXPECT_EQ(1, 1);
   EXPECT_TRUE(true);
 }
+
+TEST(TestCaseName, First) {
+	Task 1;
+	EXPECT_EQ(1, 2);
+}

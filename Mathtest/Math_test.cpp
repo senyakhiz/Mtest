ad9c976ff898359db94eee8077ef6fc22e64b79f
Ksenya <hksyusha07@gmail.com>
2026-09-17 01:20:08 +0400

    Добавлен main

--- Mathtest/Math_test.cpp
+++ Mathtest/Math_test.cpp
@@ -1,5 +1,46 @@
 #include <iostream>
 
+#include "Lib1.cpp"
+#include <cstdlib>
+#include <ctime>
+
 int main() {
-	std::cout << "Hi!" << std::endl;
+	std::srand(static_cast<unsigned>(std::time(nullptr)));
+
+	std::cout << "Choose mode:\n"
+		<< "1 - random tasks (default generator)\n"
+		<< "2 - range tasks (min..max)\n"
+		<< "3 - range tasks with fixed operation\n"
+		<< "Your choice: ";
+
+	int mode = 1;
+	std::cin >> mode;
+
+	MathTest* test = nullptr;
+
+	if (mode == 1) {
+		int count;
+		std::cout << "How many tasks? ";
+		std::cin >> count;
+		test = new MathTest(count);
+	}
+	else if (mode == 2) {
+		int count, min, max;
+		std::cout << "count min max: ";
+		std::cin >> count >> min >> max;
+		test = new MathTest(count, min, max);
+	}
+	else {
+		int count, min, max;
+		char op;
+		std::cout << "count min max operation(+ - *): ";
+		std::cin >> count >> min >> max >> op;
+		test = new MathTest(count, min, max, op);
+	}
+
+	test->run();
+	test->show_statistics();
+
+	delete test;
+	return 0;
 }
